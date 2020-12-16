daa94ffccf06093b5cdde9b2f49bd41dec0cacf7
Carson Jardine <carsonjardine@gmail.com>
2020-12-15 19:11:27 -0700

    Write validation/relation tests for each table

--- db/migrate/20201216010937_add_unit_price_to_invoice_items.rb
+++
@@ -1,7 +0,0 @@
-class AddUnitPriceToInvoiceItems < ActiveRecord::Migration[5.2]
-  def change
-    unless ActiveRecord::Base.connection.table_exists?('customers')
-      add_column :invoice_items, :unit_price, :float
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_010937) do
+ActiveRecord::Schema.define(version: 2020_12_16_005853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
