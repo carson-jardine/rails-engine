3a4cebfa83e3f4f9ec8a5f65b23f1f134fa64b82
Carson Jardine <carsonjardine@gmail.com>
2020-12-16 11:47:19 -0700

    Write merchant update test

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -54,4 +54,20 @@ describe 'Merchant API' do
       expect(created_merchant.name).to eq(merchant_params[:name])
     end
   end
+  describe 'merchants update' do
+    it "can update an existing merchant" do
+      id = create(:merchant).id
+      previous_name = Merchant.last.name
+      merchant_params = { name: "Costco" }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate(merchant_params)
+
+      merchant = Merchant.find_by(id: id)
+
+      expect(response).to be_successful
+      expect(merchant.name).to_not eq(previous_name)
+      expect(merchant.name).to eq(merchant_params[:name])
+    end
+  end
 end
