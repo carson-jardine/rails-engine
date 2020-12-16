b8021743632abda20dded9f6fe0dd439f9d98bcb
Carson Jardine <carsonjardine@gmail.com>
2020-12-15 18:13:36 -0700

    Add unit price to invoice items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_005853) do
+ActiveRecord::Schema.define(version: 2020_12_16_010937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_005853) do
     t.bigint "item_id"
     t.bigint "invoice_id"
     t.integer "quantity"
+    t.float "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
