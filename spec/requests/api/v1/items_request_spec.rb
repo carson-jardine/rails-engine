ae4b6bb93b4ed95eec1494c903fdd7de4e80ef6b
Carson Jardine <carsonjardine@gmail.com>
2020-12-15 20:07:36 -0700

    Update tests and code with item show. ALl tests pass

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,34 +1,57 @@
 require 'rails_helper'
 
 describe 'Items API' do
-  it "sends a list of items" do
-    create_list(:item, 3)
+  describe 'items index' do
+    it "sends a list of items" do
+      create_list(:item, 3)
 
-    get '/api/v1/items'
+      get '/api/v1/items'
 
-    expect(response).to be_successful
+      expect(response).to be_successful
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    items = json[:data]
+      json = JSON.parse(response.body, symbolize_names: true)
+      items = json[:data]
 
-    expect(items.count).to eq(3)
+      expect(items.count).to eq(3)
 
-    items.each do |item|
-      expect(item).to be_a(Hash)
+      items.each do |item|
+        expect(item).to be_a(Hash)
 
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_a(String)
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
 
-      expect(item).to have_key(:attributes)
+        expect(item).to have_key(:attributes)
 
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+      end
+    end
+  end
+  describe 'items show' do
+    it "can return one item by its id" do
+      id = create(:item).id
+
+      get "/api/v1/items/#{id}"
+
+      expect(response).to be_successful
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      item = json[:data][:attributes]
+      
+      expect(item).to have_key(:name)
+      expect(item[:name]).to be_a(String)
+
+      expect(item).to have_key(:description)
+      expect(item[:description]).to be_a(String)
+
+      expect(item).to have_key(:unit_price)
+      expect(item[:unit_price]).to be_a(Float)
     end
   end
 end
